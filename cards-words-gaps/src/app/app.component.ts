b4df167cf688a9d4a59f855d92084ea6a56bdbc5
JustCode7 <harrypottz85@gmail.com>
2023-12-24 01:29:33 +0100

    feat: add styles

--- cards-words-gaps/src/app/app.component.ts
+++ cards-words-gaps/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {GapTextCardComponent} from "./gap-text-card/gap-text-card.component";
+import {AnswerTextCardComponent} from "./answer-text-card/answer-text-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatCardModule, MatButtonModule, GapTextCardComponent, AnswerTextCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
