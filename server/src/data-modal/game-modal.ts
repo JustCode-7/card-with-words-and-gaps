e600869e496a85245e7c5ff1d53d2f6c4b03b6ad
JustCode7 <harrypottz85@gmail.com>
2024-04-21 13:22:30 +0200

    feat: wip data-modal

--- server/src/data-modal/game-modal.ts
+++ server/src/data-modal/game-modal.ts
@@ -1,13 +1,46 @@
-export class User {
-    constructor(private name: string) {}
+export class Spieler {
+    name: string;
+    points: number;
+    cards : string[];
+    selectedCards: string[];
+    catLord: boolean;
+
+    constructor(name:string,
+                points: number,
+                cards : string[],
+                selectedCards: string[],
+                catLord: boolean) {
+        this.name = name;
+        this.points = 0;
+        this.cards = cards;
+        this.selectedCards = selectedCards;
+        this.catLord = catLord;
+    }
 }
 
 export class Message {
-    constructor(private from: User, private content: string) {}
+    constructor(private from: Spieler, private content: string) {}
 }
 
-export class ChatMessage extends Message{
-    constructor(from: User, content: string) {
+export class AnswerMessage extends Message{
+    constructor(from: Spieler, content: string) {
         super(from, content);
     }
+}
+
+export class Game{
+    cardset: string[] = []
+    answerset: string[] = []
+    spieler: Spieler[] = []
+    gameHash: string
+    constructor(cardset: string[],
+                answerset: string[],
+                spieler: Spieler[],
+                gameHash: string) {
+        this.cardset = cardset;
+        this.answerset = answerset;
+        this.spieler = spieler;
+        this.gameHash = gameHash;
+    }
+
 }
