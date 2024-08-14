45cfc7175a239ec92813aa19c41ac9720dfb3aca
Tom Schneider <ric0.github@gmail.com>
2024-08-15 01:40:38 +0200

    fix: issues after ng update

--- cards-words-gaps/src/app/app.component.ts
+++ cards-words-gaps/src/app/app.component.ts
@@ -16,11 +16,6 @@ import {AnswerTextCardComponent} from "./components/answer-text-card/answer-text
     MatButtonModule,
     GapTextCardComponent,
     AnswerTextCardComponent,
-
-// TODO: `HttpClientModule` should not be imported into a component directly.
-// Please refactor the code to add `provideHttpClient()` call to the provider list in the
-// application bootstrap logic and remove the `HttpClientModule` import from this component.
-    HttpClientModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
